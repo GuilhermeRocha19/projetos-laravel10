5a589c52a5a9e7bf38acaa139460df819c9a1e12
GuilhermeRocha19 <guilhermepmrocha@gmail.com>
2024-06-11 11:33:29 -0300

    Criaçcao de rotas e ajustes

---
+++ projeto-laravel/app/Http/Controllers/ContaController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContaController extends Controller
+{
+    //Listar Contas
+    public function index(){
+        dd("Listar");
+    }
+
+    //Detalhes da Conta
+
+    //Carregar Formulário cadastrar novva conta
+
+    //Cadastrar no banco de dados nova conta
+
+    //Carregar o formulário editar a conta
+
+    //Editar no banco de dados a conta
+
+    //Excluir a conta do banco de dados
+
+}
--- projeto-laravel/routes/web.php
+++ projeto-laravel/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ContaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/index-conta', [ContaController::class, 'index'])->name('conta.index');
